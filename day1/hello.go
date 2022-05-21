545f99698705772be4afbd5ebfae6ce0cd910d5e
Benoit Pasquier <benoit.pasquier@shopback.com>
2022-05-21 22:13:33 +0800

    Adding default greeting + tests

--- day1/hello.go
+++ day1/hello.go
@@ -2,11 +2,13 @@ package main
 
 import "fmt"
 
+const englishGreeting = "Hello, "
+
 func Hello(name string) string {
 	if len(name) > 0 {
-		return "Hello, " + name
+		return englishGreeting + name
 	} else {
-		return "Hello World"
+		return "Hello, World"
 	}
 
 }
